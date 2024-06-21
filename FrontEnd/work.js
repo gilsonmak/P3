a6a3d49f825868d3ccf679242fd8f335284840c1
gilsonmak <gilsonmakanounou@yahoo.com>
2024-06-22 00:37:14 +0200

    feat:récupération des travaux

--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,14 +1,12 @@
 /********************************************** */
-// Récupération des données
-let workData;
-fetch("http://localhost:5678/api/works")
-    .then(response =>response.json())
-    .then((works) => {
-        workData = works;
-        console.log(workData);
-    })
-    .catch((error) => {
-        console.error("Erreur lotrs de la récupération des images", error)
-    });
-let gallery =document.querySelector(".gallery");  
-
+init ()
+function init () {
+    getWorks();  
+}
+let gallery =document.querySelector(".gallery");
+// Récupération des travaux
+function getWorks () {
+    fetch("http://localhost:5678/api/works")
+    .then(result=>result.json())
+    .then(works=>displayWorks(works))
+}
