5f9d56af00d5f1f881fbf19c2d96e64144d167b9
gilsonmak <gilsonmakanounou@yahoo.com>
2024-06-24 16:03:43 +0200

    feat: affichage des images dynamiques en javascript

--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,12 +1,37 @@
 /********************************************** */
-init ()
-function init () {
+init()
+function init() {
     getWorks();  
 }
-let gallery =document.querySelector(".gallery");
 // Récupération des travaux
 function getWorks () {
     fetch("http://localhost:5678/api/works")
     .then(result=>result.json())
     .then(works=>displayWorks(works))
+}
+// appeler la fonction pour afficher les travaux
+function displayWorks (works) {
+    const gallery = document.querySelector(".gallery")
+    gallery.innerHTML = "";
+    for(let work of works) {
+        let figure = document.createElement("figure");
+        figure.dataset.categoryId = work.categoryId
+        let img = document.createElement("img");
+        img.setAttribute("src",work.imageUrl);
+        img.setAttribute("alt",work.title);
+        let figCaption = document.createElement("figcaption");
+        figCaption.innerText = work.title;
+        figure.append(img);
+        figure.append(figCaption);
+          // gallery.append(figure);
+        console.log(figure);
+        document.querySelector(".gallery").append(figure); 
+
+
+
+        
+
+    }
+
+
 }
