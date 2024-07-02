cff126a9f5ab1329e4ddf61648e95956a25591f2
gilsonmak <gilsonmakanounou@yahoo.com>
2024-07-02 17:04:41 +0200

    feat: ajout d'une nouvelle class btnFilter

--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,15 +1,12 @@
 /********************************************** */
 // Chargement des données
-init()
-function init() {
     getWorks();
     getCategories();
-}
 // Récupération des travaux
 function getWorks () {
     fetch("http://localhost:5678/api/works")
-    .then(result=>result.json())
-    .then(works=>displayWorks(works))
+    .then(result => result.json())
+    .then(works => displayWorks(works))
 }
 // Affichage des travaux
 function displayWorks (works) {
@@ -29,10 +26,33 @@ function displayWorks (works) {
        gallery.appendChild(figureElement)
     }
 }
-
 // Récupération des travaux par catégorie via l'API
-//function getCategories () {
+function getCategories () {
     fetch("http://localhost:5678/api/categories")
-    .then(result =>result.json())
-    .then(categories=>displayCategories(categories))
+    .then( result => result.json())
+    .then(categories => displayCategories(categories))
+    console.log(categories)
+}
+function displayCategories (categories) {
+    const filters = document.querySelector(".filters");
+    filters.innerHTML ="";
+     //creation de la balise button "tous" en dehors de la boucle puisque hors catégorie
+     const bouttonFilterTous = document.createElement("button");
+     bouttonFilterTous.setAttribute("class", "btn-tous");
+     bouttonFilterTous.textContent = "Tous";
+     filters.appendChild(bouttonFilterTous)
+     // afficher les catégories
+    for(let categorie of categories) {
+        let buttons = document.createElement("button");
+        buttons.innerText = categorie.name;
+        console.log(categorie)
+        buttons.dataset.categorieId = categorie.id;
+        buttons.classList.add("btnFilter");
+        filters.appendChild(buttons)
+        // Ajout d'un évenement 
+        buttons.addEventListener ("click", (event) => {
+            console.log(event.target.dataset.categorieId)
 
+        })
+    }
+}
