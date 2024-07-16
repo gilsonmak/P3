87738c3d554ec3fd03855a3970085762b6b501f9
gilsonmak <gilsonmakanounou@yahoo.com>
2024-07-16 17:28:44 +0200

    feat: création du login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,20 +1,39 @@
-login();
-function login () {
-    const logForm = document.getElementById("form-login");
-    logForm.addEventListener("submit", function(event) {
-        event.preventDefault();
+login(); // Appel de la fonction
+function login() {
+  const logForm = document.getElementById("form-login");
+  logForm.addEventListener("submit", function (event) {
+    event.preventDefault();
     // création de l'objet user
-        const user = {
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value,
-        };
+    const user = {
+      email: event.target.querySelector("[name=email]").value,
+      password: event.target.querySelector("[name=password]").value,
+    };
+    const error = document.getElementById("error");
     // Création de la charge utile au format JSON
-        const chargeUtile = JSON.stringify(user);
+    const payload = JSON.stringify(user);
     // Appel de la fonction fetch
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: chargeUtile
-        });
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: payload,
     })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("authToken", data.token);
+          console.log(data.token);
+          alert("Connexion réussie ! Redirection...");
+          window.location.href = "index.html";
+        } else {
+          // Affichage d'un message d'erreur si les informations de connexion sont incorrectes
+          error.textContent = "Email ou mot de passe incorrect";
+          error.style.color = "red";
+        }
+      })
+      .catch((error) => {
+        console.log("Erreur lors de la connexion:", error);
+        // Affichage d'un message d'erreur si les informations de connexion sont incorrectes
+        error.style.color = "red";
+      });
+  });
 }
--- FrontEnd/work.js
+++ FrontEnd/work.js
@@ -1,65 +1,64 @@
-/********************************************** */
+/***********************************************/
 // Chargement des données
-    getWorks("All");
-    getCategories();
+getWorks("All");
+getCategories();
 // Récupération des travaux via l'API
-function getWorks (categorieId) {
-    fetch("http://localhost:5678/api/works")
-    .then(result => result.json())
-    .then(works => displayWorks(works,categorieId))
+function getWorks(categorieId) {
+  fetch("http://localhost:5678/api/works")
+    .then((result) => result.json())
+    .then((works) => displayWorks(works, categorieId));
 }
 // Affichage des travaux
-function displayWorks (works,categorieId) {
-    const gallery = document.querySelector(".gallery");
-    gallery.innerHTML ="";
-    for (let work of works) {
-        if(categorieId != "All" && categorieId != work.categoryId){
-            continue
-        }
-        let figureElement = document.createElement("figure");
-// Création de l'image
-        const imgElement = document.createElement("img");
-        imgElement.src = work.imageUrl;
-        figureElement.appendChild(imgElement);
-// Création de la légende
-        const figcaptionElement = document.createElement("figcaption")
-        figcaptionElement.innerText = work.title;
-        figureElement.appendChild(figcaptionElement)
-// Ajout de la figure dans la gallery
-       gallery.appendChild(figureElement)
+function displayWorks(works, categorieId) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  for (let work of works) {
+    if (categorieId != "All" && categorieId != work.categoryId) {
+      continue;
     }
+    let figureElement = document.createElement("figure");
+    // Création de l'image
+    const imgElement = document.createElement("img");
+    imgElement.src = work.imageUrl;
+    figureElement.appendChild(imgElement);
+    // Création de la légende
+    const figcaptionElement = document.createElement("figcaption");
+    figcaptionElement.innerText = work.title;
+    figureElement.appendChild(figcaptionElement);
+    // Ajout de la figure dans la gallery
+    gallery.appendChild(figureElement);
+  }
 }
 // Récupération des travaux par catégorie via l'API
-function getCategories () {
-    fetch("http://localhost:5678/api/categories")
-    .then( result => result.json())
-    .then(categories => displayCategories(categories))
+function getCategories() {
+  fetch("http://localhost:5678/api/categories")
+    .then((result) => result.json())
+    .then((categories) => displayCategories(categories));
 }
 // Affichage des catégories
-function displayCategories (categories) {
-    const filters = document.querySelector(".filters");
-    filters.innerHTML ="";
-     //creation de la balise button "tous" en dehors de la boucle puisque hors catégorie
-     const bouttonFilterAll = document.createElement("button");
-     bouttonFilterAll.setAttribute("class", "btn-tous");
-     bouttonFilterAll.textContent = "Tous";
-     bouttonFilterAll.dataset.categorieId = "All"
-     // Ajout d'un évenement 
-    bouttonFilterAll.addEventListener ("click", () => {
-        getWorks("All");
-        }) 
-     filters.appendChild(bouttonFilterAll)   
-// affichage les filtres par catégories
-    for(let categorie of categories) {
-        let buttons = document.createElement("button");
-        buttons.innerText = categorie.name;
-        console.log(categorie)
-        buttons.dataset.categorieId = categorie.id;
-        buttons.classList.add("btnFilter");
-        buttons.addEventListener ("click", (event) => {
-            getWorks(event.target.dataset.categorieId);
-            }) 
-        filters.appendChild(buttons)
-    }
+function displayCategories(categories) {
+  const filters = document.querySelector(".filters");
+  filters.innerHTML = "";
+  //creation de la balise button "tous" en dehors de la boucle puisque hors catégorie
+  const bouttonFilterAll = document.createElement("button");
+  bouttonFilterAll.setAttribute("class", "btn-tous");
+  bouttonFilterAll.textContent = "Tous";
+  bouttonFilterAll.dataset.categorieId = "All";
+  // Ajout d'un évenement
+  bouttonFilterAll.addEventListener("click", () => {
+    getWorks("All");
+  });
+  filters.appendChild(bouttonFilterAll);
+  // affichage les filtres par catégories
+  for (let categorie of categories) {
+    let buttons = document.createElement("button");
+    buttons.innerText = categorie.name;
+    console.log(categorie);
+    buttons.dataset.categorieId = categorie.id;
+    buttons.classList.add("btnFilter");
+    buttons.addEventListener("click", (event) => {
+      getWorks(event.target.dataset.categorieId);
+    });
+    filters.appendChild(buttons);
+  }
 }
-
