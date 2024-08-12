ffdb185cea11e88c931a7e6ceb6d4f95f68ee709
gilsonmak <gilsonmakanounou@yahoo.com>
2024-08-12 16:02:25 +0200

    feat: stylisation de la modale

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -2,6 +2,7 @@ const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 const modalGallery = document.querySelector(".modal-gallery");
 const addPhotoButton = document.querySelector("#add-photo-button");
+const backModal = document.querySelector("#back-modal");
 let dataLoaded = false;
 // Définition de la fonction toggleModal
 function toggleModal() {
@@ -37,6 +38,13 @@ async function begin() {
       logoSupp.appendChild(icon);
       logoSupp.id = "suppPhoto";
       logoSupp.dataset.workId = work.id;
+
+      // Ajout de l'écouteur de click pour suppression
+      logoSupp.addEventListener("click", async (event) => {
+        event.preventDefault();
+        const workId = event.currentTarget.dataset.workId;
+        await deleteWork(workId, figureElement);
+      });
       figureElement.appendChild(logoSupp);
       // Ajout de la figure dans la gallery
       modalGallery.appendChild(figureElement);
@@ -45,6 +53,68 @@ async function begin() {
     console.error("Error fetching works:", error);
   }
 }
+// Fonction pour supprimer un work via l'API
+async function deleteWork(workId, workElement) {
+  const authToken = localStorage.getItem("authToken");
+  if (!authToken) {
+    console.log("Erreur : Accès non autorisé à l'API 'delete'");
+    return;
+  }
+  try {
+    let response = await fetch(`http://localhost:5678/api/works/${workId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      console.log(`L'objet avec l'id ${workId} a été supprimé.`);
+      begin();
+      getWorks("All");
+    } else {
+      console.error(
+        `Erreur de suppression pour l'objet avec l'id ${workId}:`,
+        response.status
+      );
+    }
+  } catch (error) {
+    console.error(
+      `Erreur lors de la suppression de l'objet avec l'id ${workId}:`,
+      error
+    );
+  }
+}
+
 addPhotoButton.addEventListener("click", () => {
-  console.log(" le bouton a été cliqué !!!!");
+  // Masquer les éléments
+  document.querySelector(".modal-gallery").style.display = "none";
+  document.querySelector(".modal h3").style.display = "none";
+  document.querySelector("#add-photo-button").style.display = "none";
+
+  // Afficher les éléments
+  document.querySelector("#back-modal").style.display = "block";
+  document.querySelector(".modal-h3").style.display = "block";
+  document.querySelector(".modal-new-photo").style.display = "block";
+  document.querySelector(".modal-new-category").style.display = "flex";
+  document.querySelector(".selectCategorie").style.display = "block";
+  document.querySelector("#btn-valider").style.display = "block";
+});
+
+document.querySelector(".photo-file").addEventListener("click", () => {
+  document.querySelector("#avatar").click();
+});
+backModal.addEventListener("click", () => {
+  // Afficher les éléments
+  document.querySelector(".modal-gallery").style.display = "flex";
+  document.querySelector(".modal h3").style.display = "flex";
+  document.querySelector("#add-photo-button").style.display = "flex";
+
+  // Masquer les éléments
+  document.querySelector("#back-modal").style.display = "none";
+  document.querySelector(".modal-h3").style.display = "none";
+  document.querySelector(".modal-new-photo").style.display = "none";
+  document.querySelector(".modal-new-category").style.display = "none";
+  document.querySelector(".selectCategorie").style.display = "none";
+  document.querySelector("#btn-valider").style.display = "none";
 });
